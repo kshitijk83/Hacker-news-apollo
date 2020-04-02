003f91b898b931743f8857c4dce7dc49dd50ba2b
kshitijk83 <kshitijk83@gmail.com>
2020-04-02 20:51:35 +0530

    subscription done

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -25,6 +25,52 @@ export const FEED_QUERY=gql`
         }
     }
 `
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
 
 class LinkList extends Component {
 
@@ -35,13 +81,41 @@ class LinkList extends Component {
         store.writeQuery({query: FEED_QUERY, data});
     }
 
+    _subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, {subscriptionData})=>{
+                if(!subscriptionData) return prev;
+                const newLink = subscriptionData.data.newLink;
+                const exist = prev.feed.links.find(({id})=>id===newLink.id);
+                if(exist) return prev;
+
+                return Object.assign({}, prev, {
+                    feed:{
+                        links: [newLink, ...prev.feed.links],
+                        count: prev.feed.links.length+1,
+                        __typename: prev.feed.__typename
+                    }
+                })
+            }
+        })
+    }
+
+    _subscribeToNewVotes = subscribeToMore => {
+        subscribeToMore({
+          document: NEW_VOTES_SUBSCRIPTION
+        })
+    }      
+
   render() {
 
     return (
         <Query query={FEED_QUERY}>
-            {({loading, error, data})=>{
+            {({loading, error, data, subscribeToMore})=>{
                 if(loading) return <div>Fetching</div>
                 if(error) return <div>error</div>
+                this._subscribeToNewLinks(subscribeToMore)
+                this._subscribeToNewVotes(subscribeToMore)
                 const linksToRender = data.feed.links
                 return(
                     <div>
--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { BrowserRouter } from 'react-router-dom';
 import { setContext } from 'apollo-link-context';
 import { AUTH_TOKEN } from './constants';
+
 
 const authLink = setContext((_, {headers})=>{
     const token=localStorage.getItem(AUTH_TOKEN);
@@ -25,8 +29,28 @@ const authLink = setContext((_, {headers})=>{
 const httpLink = createHttpLink({
     uri: "http://localhost:4000"
 });
+
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000',
+    options: {
+        reconnect: true,
+        connectionParams:{
+            authToken: localStorage.getItem(AUTH_TOKEN)
+        }
+    }
+});
+
+const link = split(
+    ({query})=>{
+        const { kind, operation } = getMainDefinition(query);
+        return kind==="OperationDefinition"&&operation==="subscription"
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 });
 
